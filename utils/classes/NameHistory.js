17052a0d047ee207899d582c09fbc34034a8b3dd
max <max.0x71@gmail.com>
2021-04-05 16:54:29 +0100

    added page function

--- utils/classes/NameHistory.js
+++ utils/classes/NameHistory.js
@@ -12,6 +12,45 @@ class Account {
 
         return this
     }
+
+    /**
+     * @returns {Map<Number, Array<String>}
+     * @param {Number} size size of the pages for name history. default: 7
+     * @param {String} format format to use when converting previous name to string. default: "$username | $date"
+     */
+    toPages(size, format) {
+        const pages = new Map()
+
+        if (!size) {
+            size = 7
+        }
+
+        if (!format) {
+            format = "$username | $date"
+        }
+
+        this.history.forEach((item) => {
+            const value = item.toString(format)
+
+            if (pages.size == 0) {
+                const value1 = [value]
+
+                pages.set(1, value1)
+            } else {
+                const lastPage = pages.size
+
+                if (pages.get(lastPage).length >= size) {
+                    const value1 = [value]
+
+                    pages.set(lastPage + 1, value1)
+                } else {
+                    pages.get(lastPage).push(value)
+                }
+            }
+        })
+
+        return pages
+    }
 }
 
 exports.Account = Account
