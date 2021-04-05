e6313253d0ee7e2ad9323b25e1d7bd16101dab9d
max <max.0x71@gmail.com>
2021-04-05 20:17:39 +0100

    attempting to fix intellisense for npm package

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const { Account } = require("./utils/classes/NameHistory")
 const { getNameHistory } = require("./utils/utils")
 
 module.exports = {
-    getNameHistory: getNameHistory,
+    /**
+     *
+     * @param {String} username username of the user to get previous names for
+     * @returns {Promise<Account>}
+     */
+    getNameHistory: async function (username) {
+        return await getNameHistory(username)
+    },
 }
