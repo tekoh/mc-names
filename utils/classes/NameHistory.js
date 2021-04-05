c3cacda8cdc6cf64d2b35c9a17d86926028bc209
max <max.0x71@gmail.com>
2021-04-05 15:29:33 +0100

    created basic classes

---
+++ utils/classes/NameHistory.js
@@ -0,0 +1,42 @@
+class Account {
+    /**
+     * @returns {Account}
+     * @param {String} uuid uuid of user
+     * @param {String} current current username of user
+     * @param {Array<PreviousName>} past previous names of the user
+     */
+    constructor(uuid, current, past) {
+        this.uuid = uuid
+        this.username = current
+        this.pastNames = past
+        
+        return this
+    }
+}
+
+exports.Account = Account
+
+class PreviousName {
+    /**
+     * 
+     * @param {String} username the username that the account was changed to
+     * @param {String} date the date that the username was changed
+     * @returns {PreviousName}
+     */
+    constructor(username, date) {
+        this.username = username
+        this.date = date
+
+        return this
+    }
+
+    /**
+     * 
+     * @returns {String} formatted version of the date
+     */
+    getFormattedDate() {
+        return new Date(this.date).toUTCString()
+    }
+}
+
+exports.PreviousName = PreviousName
