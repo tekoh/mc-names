83d969a70767b4a3eaac2c99d44121a9cf944c19
max <max.0x71@gmail.com>
2021-04-05 20:21:14 +0100

    trying to fix it again aaaa

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const { Account } = require("./utils/classes/NameHistory")
 const { getNameHistory } = require("./utils/utils")
 
-module.exports = {
-    /**
-     *
-     * @param {String} username username of the user to get previous names for
-     * @returns {Promise<Account>}
-     */
-    getNameHistory: async function (username) {
-        return await getNameHistory(username)
-    },
-}
+exports.getNameHistory = getNameHistory
