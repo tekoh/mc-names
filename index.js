7b446767ca3a0dc00a7d6e1574d3e55977bb3d6b
max <max.0x71@gmail.com>
2021-04-10 15:23:57 +0100

    exports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Account } = require("./utils/classes/NameHistory")
 const { getNameHistory } = require("./utils/names")
+const { getSkin } = require("./utils/skins")
 const { getUUID } = require("./utils/utils")
 
 exports.getNameHistory = getNameHistory
 exports.getUUID = getUUID
+exports.getSkin = getSkin
